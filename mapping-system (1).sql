430666f043592e5ed01f77a176079abd6a4d6ea0
MDSCSI0\ryan.yatasa <wbear031098@gmail.com>
2022-06-14 19:21:19 +0800

    update

--- mapping-system (1).sql
+++ mapping-system (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2022 at 01:39 PM
+-- Generation Time: Jun 14, 2022 at 01:20 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -44,15 +44,17 @@ CREATE TABLE `cemetery` (
   `cemetery_no` int(11) NOT NULL,
   `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `cemetery`
 --
 
-INSERT INTO `cemetery` (`id`, `cemetery_no`, `status`, `created_at`, `updated_at`) VALUES
-(1, 47, 'reserve', NULL, NULL);
+INSERT INTO `cemetery` (`id`, `cemetery_no`, `status`, `created_at`, `updated_at`, `created_by`) VALUES
+(1, 12, 'occupied', NULL, '2022-06-13 15:45:42', 'admin'),
+(2, 12, 'reserve', NULL, NULL, 'admin');
 
 -- --------------------------------------------------------
 
@@ -118,11 +120,15 @@ CREATE TABLE `peoples` (
   `cemetery_no` int(11) NOT NULL,
   `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middle_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `user_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `grave_no` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `born_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `die_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `block_no` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_of_lot` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -131,8 +137,18 @@ CREATE TABLE `peoples` (
 -- Dumping data for table `peoples`
 --
 
-INSERT INTO `peoples` (`id`, `cemetery_no`, `first_name`, `last_name`, `user_image`, `grave_no`, `born_date`, `die_date`, `block_no`, `created_at`, `updated_at`) VALUES
-(1, 47, 'Juan', 'Ponze', 'img', '0', '2022-06-10 02:41:06', '2022-06-16 02:41:06', '1', NULL, NULL);
+INSERT INTO `peoples` (`id`, `cemetery_no`, `first_name`, `last_name`, `middle_name`, `user_image`, `grave_no`, `born_date`, `die_date`, `block_no`, `type_of_lot`, `status`, `created_by`, `created_at`, `updated_at`) VALUES
+(1, 368, 'Kemerut', 'Osme', 'asdsdasdasd', 'img', '0', '2022-06-23 23:51:35', '2022-06-06 23:32:40', '5', 'Two Lawn Lot', 'reserve', 'admin', NULL, '2022-06-13 23:32:40'),
+(2, 369, 'Juan', 'Ponze', 'asd', 'img', '0', '2022-06-13 23:51:35', '2022-06-08 23:51:35', '5', 'Two Lawn Lot', 'occupied', 'admin', NULL, '2022-06-13 23:51:35'),
+(3, 369, 'ad', 'ad', 'ad', 'img', '0', '2022-06-14 08:20:36', '2022-06-27 23:46:45', '5', 'Two Lawn Lot', 'occupied', 'admin', NULL, '2022-06-14 00:20:36'),
+(4, 368, 'Juan', 'Ponze', 'asd', 'img', '0', '2022-06-14 08:20:45', '2022-06-13 23:43:02', '5', 'Senior Estate', 'occupied', 'admin', NULL, '2022-06-14 00:20:45'),
+(5, 455, 'Juan', 'Ponze', 'asd', 'img', '0', '2022-06-16 23:43:16', '2022-06-07 23:43:16', '6', 'Senior Estate', 'reserve', 'admin', NULL, NULL),
+(6, 435, 'Juan', 'Ponze', 'asd', 'img', '0', '2022-06-02 23:50:32', '2022-06-05 23:50:32', '6', 'Junior Estat', 'reserve', 'admin', NULL, NULL),
+(7, 104, 'Juan', 'Ponze', 'asd', 'img', '0', '2022-06-01 23:58:18', '2022-06-05 23:58:18', '2', 'Senior Estate', 'reserve', 'user', NULL, NULL),
+(8, 65, 'Bleh', 'Bleh', 'asd', 'img', '0', '2022-06-14 00:18:52', '2022-06-14 00:18:52', '2', 'Memorial Garden', 'reserve', 'admin', NULL, NULL),
+(9, 104, 'Ryan', 'Blissed', 'asdsdasdasd', 'img', '0', '2022-06-14 00:29:25', '2022-06-14 00:29:25', '2', 'Single Niche', 'reserve', 'admin', NULL, NULL),
+(10, 1, 'Joe', 'an', 'Ki', 'img', '0', '2022-06-14 11:11:01', '2022-06-14 03:09:40', '1', 'Memorial Garden', 'occupied', 'admin', NULL, '2022-06-14 03:11:01'),
+(11, 1, 'Zo', 'An', 'P', 'img', '0', '2022-06-14 03:10:27', '2022-06-14 03:10:27', '1', 'Senior Estate', 'reserve', 'admin', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -175,7 +191,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `role`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin', 'Admin@sample.com', NULL, '$2y$10$FfMbSU5KoiuVu4FAwrVIreM6Fib6LjmcadQAj34fd5hczPSNM4bwa', NULL, '2022-06-09 17:54:17', '2022-06-09 17:54:17'),
+(1, 'Admin', 'admin', 'admin@sample.com', NULL, '$2y$10$FfMbSU5KoiuVu4FAwrVIreM6Fib6LjmcadQAj34fd5hczPSNM4bwa', NULL, '2022-06-09 17:54:17', '2022-06-09 17:54:17'),
 (2, 'user', 'user', 'User@sample.com', NULL, '$2y$10$FfMbSU5KoiuVu4FAwrVIreM6Fib6LjmcadQAj34fd5hczPSNM4bwa', NULL, '2022-06-11 07:03:15', '2022-06-11 07:03:16'),
 (3, 'test', NULL, 'test@gmail.com', NULL, '$2y$10$sbrXoF9cQhk6zW4cScMpTOBcKo1YnIpAMN9oFsOYjeRv/k1eSbitK', NULL, '2022-06-11 06:06:24', '2022-06-11 06:06:24');
 
@@ -249,7 +265,7 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `cemetery`
 --
 ALTER TABLE `cemetery`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -267,7 +283,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `peoples`
 --
 ALTER TABLE `peoples`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
